7e37ede47f1bcabf8f9716559450ca06487932de
CHENG-JE <lcjbao@gmail.com>
2025-06-13 00:04:15 +0800

    add class Arrow

--- weapon.py
+++ weapon.py
@@ -1,4 +1,3 @@
-# laser.py
 import pygame
 
 class Laser(pygame.sprite.Sprite):
@@ -24,4 +23,34 @@ class Laser(pygame.sprite.Sprite):
         # 如果飛出畫面就自動刪除
         if (self.rect.right < 0 or self.rect.left > 800 or 
             self.rect.bottom < 0 or self.rect.top > 600):
+            self.kill()
+
+
+class Arrow(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction, speed=15, damage=40):
+        super().__init__()
+        # 載入圖片
+        self.image = pygame.image.load("assets/weapon/arrow.png").convert_alpha()
+        # 調整圖片大小
+        self.image = pygame.transform.scale(self.image, (40, 10))
+        
+        self.direction = direction
+        self.speed = speed
+        self.damage = damage
+        
+        # 根據方向翻轉圖片
+        if self.direction == "left":
+            self.image = pygame.transform.flip(self.image, True, False) # True 代表水平翻轉, False 代表不垂直翻轉
+        
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def update(self):
+        # 移動邏輯和 Laser 一樣
+        if self.direction == "right":
+            self.rect.x += self.speed
+        elif self.direction == "left":
+            self.rect.x -= self.speed
+        
+        # 如果飛出畫面就自動刪除
+        if self.rect.right < 0 or self.rect.left > 800:
             self.kill()
