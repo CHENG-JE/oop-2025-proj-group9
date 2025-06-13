aa691a7c8c23af035c9e043a439c49c672b6c643
CHENG-JE <lcjbao@gmail.com>
2025-06-14 02:38:12 +0800

    update droplet box area

--- level1/droplet.py
+++ level1/droplet.py
@@ -11,15 +11,21 @@ class Droplet(pygame.sprite.Sprite):
         self.exp = 0
         self.money = 0
         
-        # 圖像與旋轉
+        # 視覺圖像 (可以旋轉)
         self.original_image = pygame.image.load("assets/player/droplet.png").convert_alpha()
         self.original_image = pygame.transform.scale(self.original_image, (34, 17))
         self.image = self.original_image
-        
-        # 位置與移動
         self.rect = self.image.get_rect(center=start_pos)
-        self.pos = pygame.math.Vector2(start_pos) # 使用向量來處理精確位置
-        self.speed = 3 # 180 像素/秒
+        
+        # === 新增：物理碰撞框 (Hitbox) ===
+        # Hitbox 的尺寸是固定的，比視覺圖像略小，且不會旋轉
+        hitbox_size = (16, 16) 
+        self.hitbox = pygame.Rect(0, 0, hitbox_size[0], hitbox_size[1])
+        self.hitbox.center = self.rect.center
+
+        # 位置與移動 (使用向量來處理精確位置)
+        self.pos = pygame.math.Vector2(start_pos)
+        self.speed = 3
 
     def update(self, keys, walls):
         # 處理輸入並獲取移動向量
@@ -29,41 +35,46 @@ class Droplet(pygame.sprite.Sprite):
         if keys[pygame.K_a]: move_vector.x = -1
         if keys[pygame.K_d]: move_vector.x = 1
 
-        # 如果有移動，才進行旋轉和位置更新
         if move_vector.length_squared() > 0:
-            move_vector.normalize_ip() # 將向量長度設為1，確保斜向移動不會比較快
+            move_vector.normalize_ip()
 
-            # 旋轉圖像
-            # atan2 的參數是 (y, x)，回傳弧度。我們轉換為角度
-            # Pygame 的角度是逆時針為正，且 0 度朝右。我們的圖朝上，所以要減 90 度
+            # --- 只旋轉視覺圖像 ---
             angle = move_vector.angle_to(pygame.math.Vector2(0, -1))
             self.image = pygame.transform.rotate(self.original_image, angle)
-            self.rect = self.image.get_rect(center=self.rect.center)
+            self.rect = self.image.get_rect(center=self.hitbox.center) # 視覺圖像的中心永遠對齊 Hitbox 的中心
             
-            # 移動並處理碰撞
+            # --- 移動與碰撞改為基於 Hitbox ---
             self.move_and_collide(move_vector, walls)
 
     def move_and_collide(self, move_vector, walls):
-        # 分別處理 X 和 Y 軸的移動與碰撞
-        # X 軸
+        # --- 使用 Hitbox 進行所有移動和碰撞計算 ---
+        
+        # X 軸移動
         self.pos.x += move_vector.x * self.speed
-        self.rect.centerx = round(self.pos.x)
-        for wall in pygame.sprite.spritecollide(self, walls, False):
-            if move_vector.x > 0: # 向右移動
-                self.rect.right = wall.rect.left
-            elif move_vector.x < 0: # 向左移動
-                self.rect.left = wall.rect.right
-            self.pos.x = self.rect.centerx
+        self.hitbox.centerx = round(self.pos.x)
+        # X 軸碰撞
+        for wall in walls:
+            if self.hitbox.colliderect(wall.rect):
+                if move_vector.x > 0: # 向右移動
+                    self.hitbox.right = wall.rect.left
+                elif move_vector.x < 0: # 向左移動
+                    self.hitbox.left = wall.rect.right
+                self.pos.x = self.hitbox.centerx # 碰撞後，校正浮點數位置
 
-        # Y 軸
+        # Y 軸移動
         self.pos.y += move_vector.y * self.speed
-        self.rect.centery = round(self.pos.y)
-        for wall in pygame.sprite.spritecollide(self, walls, False):
-            if move_vector.y > 0: # 向下移動
-                self.rect.bottom = wall.rect.top
-            elif move_vector.y < 0: # 向上移動
-                self.rect.top = wall.rect.bottom
-            self.pos.y = self.rect.centery
+        self.hitbox.centery = round(self.pos.y)
+        # Y 軸碰撞
+        for wall in walls:
+            if self.hitbox.colliderect(wall.rect):
+                if move_vector.y > 0: # 向下移動
+                    self.hitbox.bottom = wall.rect.top
+                elif move_vector.y < 0: # 向上移動
+                    self.hitbox.top = wall.rect.bottom
+                self.pos.y = self.hitbox.centery
+        
+        # --- 最後，讓視覺圖像的中心跟隨 Hitbox 的中心 ---
+        self.rect.center = self.hitbox.center
             
     def take_damage(self, amount):
         self.blood = max(0, self.blood - amount)
