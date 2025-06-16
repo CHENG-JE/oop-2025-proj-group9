6662ca693d29f3d66a13ebf78f4ffabf9528c608
CHENG-JE <lcjbao@gmail.com>
2025-06-16 21:54:16 +0800

    fixed the problem with no portal

--- level1/portal.py
+++ level1/portal.py
@@ -13,6 +13,6 @@ class Portal(pygame.sprite.Sprite):
 
         # === 改正：將尺寸從 cell_size 改為固定的 (30, 30) ===
         self.image = pygame.transform.scale(self.image, (30, 30))
-        
+            
         # self.rect 會根據上面縮放後的 image 自動調整大小
         self.rect = self.image.get_rect(topleft=(x, y))
