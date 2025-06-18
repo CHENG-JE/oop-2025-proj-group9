6726b3bcabda9b6c9f33a834dfc5b64bcfb79f08
lwc-ed <edward.mg12@nycu.edu.tw>
2025-06-18 09:27:56 +0800

    modified lev1 font size

--- level1/level1_game.py
+++ level1/level1_game.py
@@ -29,7 +29,7 @@ maze_timer = 0
 lev1_bg = pygame.transform.scale(pygame.image.load("assets/background/level1.jpeg"), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def show_game_title(screen):
-    font = pygame.font.SysFont("arial", 64)
+    font = pygame.font.SysFont("arial", 50)
     text_surface = font.render("Moving Maze!", True, (255, 255, 255))
     text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
     screen.fill((0, 0, 0))
